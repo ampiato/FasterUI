7b73a2a5e9ceff906111e96b0ca1703bd3fc6554
Tomas Krejci <tomas.krejci@ampiato.com>
2024-08-19 17:55:48 +0200

    Skeleton docs. (#2)
    * Skeleton docs.
    
    * Add deploy step
    
    * Build storybook in docs

--- Earthfile
+++ Earthfile
@@ -1,4 +1,15 @@
 VERSION 0.8
 
-all:
-    BUILD ./python+all
+ci:
+    BUILD ./python+ci
+    BUILD ./python+ci
+
+
+deploy-docs:
+    FROM ./docs+deps
+
+    # BUILD ./component-library+storybook
+    COPY ./component-library+storybook/storybook-static ./public/storybook
+
+    RUN --secret NETLIFY_AUTH_TOKEN --secret NETLIFY_SITE_ID netlify build --context production
+    RUN --push --secret NETLIFY_AUTH_TOKEN --secret NETLIFY_SITE_ID netlify deploy --prod
---
+++ docs/next.config.mjs
@@ -0,0 +1,20 @@
+import withMarkdoc from '@markdoc/next.js'
+
+import withSearch from './src/markdoc/search.mjs'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
+  async rewrites() {
+    return [
+      {
+        source: '/storybook',
+        destination: '/storybook/index.html',
+      },
+    ]
+  }
+}
+
+export default withSearch(
+  withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig),
+)
